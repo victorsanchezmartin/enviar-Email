c209a8002886dd31571c32a5f1e8f4a8c64583da
Victor <lacvictorsanchezmartin@gmail.com>
2023-01-02 17:55:00 +0100

    Subida inicial

---
+++ app/src/main/java/com/example/enviarformulario/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.enviarformulario
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var boton = findViewById<Button>(R.id.btEnviar)
+        var etDestinatario = findViewById<EditText>(R.id.etDestinatario)
+        var etAsunto = findViewById<EditText>(R.id.etAsunto)
+        var etMensaje = findViewById<EditText>(R.id.etMensaje)
+
+        var destinatario = etDestinatario.text
+        var asunto = etAsunto.text
+        var mensaje = etMensaje.text
+
+        boton.setOnClickListener {
+            val intentEmail = Intent(Intent.ACTION_SEND)
+            intentEmail.type = "plain/text"
+            intentEmail.putExtra(Intent.EXTRA_SUBJECT, asunto)
+            intentEmail.putExtra(Intent.EXTRA_TEXT, mensaje)
+            intentEmail.putExtra(Intent.EXTRA_EMAIL, arrayOf(destinatario))
+            startActivity(Intent.createChooser(intentEmail, "Elige cliente de correo"))
+        }
+    }
+}
